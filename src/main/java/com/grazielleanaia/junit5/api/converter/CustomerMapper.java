37a8bb7a2f4dfc8dfd8e892518147a28d06483c2
Grazielle <grazielle.anaia@gmail.com>
2025-04-10 14:00:05 -0400

    Added Controller and yaml file

--- src/main/java/com/grazielleanaia/junit5/api/converter/CustomerMapper.java
+++ src/main/java/com/grazielleanaia/junit5/api/converter/CustomerMapper.java
@@ -5,9 +5,13 @@ import com.grazielleanaia.junit5.api.request.CustomerRequestDTO;
 import com.grazielleanaia.junit5.api.response.CustomerResponseDTO;
 import com.grazielleanaia.junit5.infrastructure.entity.CustomerEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
+
+    @Mapping(source = "id", target = "id")
+
 
     public CustomerEntity convertToCustomerEntity(CustomerRequestDTO customerRequestDTO);
 
--- src/main/java/com/grazielleanaia/junit5/infrastructure/repository/CustomerRepository.java
+++ src/main/java/com/grazielleanaia/junit5/infrastructure/repository/CustomerRepository.java
@@ -3,9 +3,13 @@ package com.grazielleanaia.junit5.infrastructure.repository;
 import com.grazielleanaia.junit5.infrastructure.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
     CustomerEntity findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
